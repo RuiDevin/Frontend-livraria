3eea725dc05ef3d68eec6a8ae54bdf05ae96e437
luizatorrens <Luiza.torrens.2006@gmail.com>
2022-07-18 15:19:54 -0300

    ok

--- src/views/LivrosView.vue
+++ src/views/LivrosView.vue
@@ -13,7 +13,11 @@ export default {
   async created() {
     await this.buscarTodosOsAutores();
     await this.buscarTodasAsCategorias();
-    await this.buscarTodosAsEditoras();
+    await this.buscarTodasAsEditoras();
+    const livros = await axios.get(
+      "http://localhost:4000/livros?expand=categoria&expand=editora&expand=autor"
+    );
+    this.livros = livros.data;
   },
 
   methods: {
@@ -21,13 +25,38 @@ export default {
       const autores = await axios.get("http://localhost:4000/autores");
       this.autores = autores.data;
     },
-    async buscarTodosAsCategorias() {
+    async buscarTodasAsCategorias() {
       const categorias = await axios.get("http://localhost:4000/categorias");
       this.categorias = categorias.data;
     },
-    async buscarTodosAsEditoras() {
+    async buscarTodasAsEditoras() {
       const editoras = await axios.get("http://localhost:4000/editoras");
       this.editoras = editoras.data;
+    },
+    async buscarTodosOsLivros() {
+      const livros = await axios.get("http://localhost:4000/livros");
+      this.livros = livros.data;
+    },
+
+    async salvar() {
+      const novo_livro = await axios.post(
+        "http://localhost:4000/livros",
+        this.livro
+      );
+      this.livros.push(novo_livro.data);
+      this.livro = {};
+    },
+    async excluir(livro) {
+      await axios.delete(`http://localhost:4000/livros/${livro.id}`);
+      const indice = this.livros.indexOf(livro);
+      this.livros.splice(indice, 1);
+    },
+    async atualizarLivro(livro) {
+      const response = await axios.put(
+        `http://localhost:4000/livros/${livro.id}`,
+        livro,
+      );
+      return response.data;
     },
   },
 };
@@ -37,36 +66,36 @@ export default {
   <div class="position-absolute top-50 start-50 translate-middle">
     <div class="formulario">
       <div>
-        <h2 class="title" >Gerencimento de Livros</h2>
+        <h2 class="title">Gerencimento de Livros</h2>
       </div>
       <div class="input-group container">
         <input
           class="col form-control d-flex"
           type="text"
-          v-model="novo_livro"
+          v-model="livro.nome"
           @keyup.enter="salvar"
           placeholder="Livros"
         />
-        <input
-          class="col form-control d-flex"
-          type="text"
-          v-model="novo_autor"
-          @keyup.enter="salvar"
-          placeholder="Autores"
-        />
-        <select v-model="livro.categoriaId" @keyup.enter="salvar">
-        <option v-for="categoria in categorias" :key="categoria.id" :value="categoria.id">
-        {{ categoria.nome }}
-        </option>
+        <select v-model="livro.autorId" @keyup.enter="salvar">
+          <option v-for="autor in autores" :key="autor.id" :value="autor.id">
+            {{ autor.nome }}
+          </option>
         </select>
 
-        <select class="form-select" v-model="nova_editora">
-          <option value="" class="col form-control d-flex" disabled>Editora</option>
-          <option value="Editora 1">Editora 1</option>
-          <option value="Editora 2">Editora 2</option>
-          <option value="Editora 3">Editora 3</option>
-          <option value="Editora 4">Editora 4</option>
-          <option value="Editora 5">Editora 5</option>
+        <select v-model="livro.categoriaId" @keyup.enter="salvar">
+          <option
+            v-for="categoria in categorias"
+            :key="categoria.id"
+            :value="categoria.id"
+          >
+            {{ categoria.nome }}
+          </option>
+        </select>
+
+        <select v-model="livro.editoraId" @keyup.enter="salvar">
+          <option v-for="editora in editoras" :key="editora.id" :value="editora.id">
+            {{ editora.nome }}
+          </option>
         </select>
 
         <button class="btn btn_save" @click="salvar">Salvar</button>
@@ -87,10 +116,10 @@ export default {
         <tbody>
           <tr v-for="livro in livros" :key="livro.id">
             <td>{{ livro.id }}</td>
-            <td>{{ livro.nome }}</td>
+            <td>{{ livro.title }}</td>
             <td>{{ livro.autorId }}</td>
-            <td>{{ livro.categoriaId }}</td>
-            <td>{{ livro.editoraId }}</td>
+            <td>{{ livro.categoryId }}</td>
+            <td>{{ livro.publisherId }}</td>
 
             <td class="button-group d-flex salvar_editar">
               <button class="btn btn-primary" @click="alerta(livro)">Editar</button>
